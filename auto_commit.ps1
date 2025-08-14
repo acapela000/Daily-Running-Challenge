8787179026a44c57c80af639d9ce91dce38fac0f
thao <mrtrados@gmail.com>
2025-08-14 20:18:36 +0900

    chore(auto_commit.ps1): updated\n auto_commit.ps1 | 10 ++++++----  1 file changed, 6 insertions(+), 4 deletions(-)

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -10,11 +10,13 @@ $files = git status --porcelain | ForEach-Object {
 }
 
 foreach ($file in $files) {
+    # Get a short diff summary for the file
+    $diff = git diff --stat "$($file.File)"
     switch ($file.Status) {
-        'M' { $msg = "chore: update $($file.File)" }
-        'A' { $msg = "feat: add $($file.File)" }
-        '??' { $msg = "feat: add new file $($file.File)" }
-        default { $msg = "chore: update $($file.File)" }
+        'M' { $msg = "chore($($file.File)): updated\n$diff" }
+        'A' { $msg = "feat($($file.File)): added\n$diff" }
+        '??' { $msg = "feat($($file.File)): new file\n$diff" }
+        default { $msg = "chore($($file.File)): updated\n$diff" }
     }
     git add "$($file.File)"
     git commit -m "$msg"
