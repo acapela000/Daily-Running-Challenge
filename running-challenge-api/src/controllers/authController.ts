47ba4c6689bc5f0e7ee87298bb19c444be120c65
thao <mrtrados@gmail.com>
2025-08-15 20:00:04 +0900

    chore(running-challenge-api/src/controllers/authController.ts): updated\n running-challenge-api/src/controllers/authController.ts | 16 +++++-----------  1 file changed, 5 insertions(+), 11 deletions(-)

--- running-challenge-api/src/controllers/authController.ts
+++ running-challenge-api/src/controllers/authController.ts
@@ -1,27 +1,21 @@
 import { Request, Response } from "express";
-import AuthService from "@/services/authService";
+import AuthService from "../services/authService";
 
 class AuthController {
-  private authService: AuthService;
-
-  constructor() {
-    this.authService = new AuthService();
-  }
-
   public async register(req: Request, res: Response): Promise<void> {
     try {
-      const user = await this.authService.register(req.body);
+      const user = await AuthService.register(req.body);
       res.status(201).json(user);
-    } catch (error) {
+    } catch (error: any) {
       res.status(400).json({ message: error.message });
     }
   }
 
   public async login(req: Request, res: Response): Promise<void> {
     try {
-      const token = await this.authService.login(req.body);
+      const token = await AuthService.login(req.body);
       res.status(200).json({ token });
-    } catch (error) {
+    } catch (error: any) {
       res.status(401).json({ message: error.message });
     }
   }
