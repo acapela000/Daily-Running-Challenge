0faea02c1dc94d318686cc00a9e9bfca00a44cc7
thao <mrtrados@gmail.com>
2025-08-15 20:00:06 +0900

    chore(running-challenge-api/src/routes/challengeRoutes.ts): updated\n running-challenge-api/src/routes/challengeRoutes.ts | 21 +++++++++++++--------  1 file changed, 13 insertions(+), 8 deletions(-)

--- running-challenge-api/src/routes/challengeRoutes.ts
+++ running-challenge-api/src/routes/challengeRoutes.ts
@@ -1,13 +1,18 @@
-import { Router } from 'express';
-import ChallengeController from '../controllers/challengeController';
+import { Router } from "express";
+import { ChallengeController } from "../controllers/challengeController";
 
 const router = Router();
 const challengeController = new ChallengeController();
 
-// CRUD operations for challenges
-router.post('/', challengeController.createChallenge.bind(challengeController));
-router.get('/', challengeController.getChallenges.bind(challengeController));
-router.put('/:id', challengeController.updateChallenge.bind(challengeController));
-router.delete('/:id', challengeController.deleteChallenge.bind(challengeController));
+router.post("/", challengeController.createChallenge.bind(challengeController));
+router.get("/", challengeController.getChallenges.bind(challengeController));
+router.put(
+  "/:id",
+  challengeController.updateChallenge.bind(challengeController)
+);
+router.delete(
+  "/:id",
+  challengeController.deleteChallenge.bind(challengeController)
+);
 
 export default router;
