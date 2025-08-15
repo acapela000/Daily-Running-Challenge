59d279633d6419824975d4f245a44994c0fdbfc9
thao <mrtrados@gmail.com>
2025-08-15 20:00:04 +0900

    chore(running-challenge-api/src/db/index.ts): updated\n running-challenge-api/src/db/index.ts | 19 ++++---------------  1 file changed, 4 insertions(+), 15 deletions(-)

--- running-challenge-api/src/db/index.ts
+++ running-challenge-api/src/db/index.ts
@@ -1,17 +1,6 @@
-import { Pool } from "pg";
+import { PrismaClient } from "@prisma/client";
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-});
+const prisma = new PrismaClient();
 
-export const query = (text: string, params?: any[]) => {
-  return pool.query(text, params);
-};
-
-export default {
-  query,
-};
+export { prisma as db };
+export default prisma;
