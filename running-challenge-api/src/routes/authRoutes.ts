6a4c29c12637c84a60eb89d257416b423110b19d
thao <mrtrados@gmail.com>
2025-08-15 20:00:05 +0900

    chore(running-challenge-api/src/routes/authRoutes.ts): updated\n running-challenge-api/src/routes/authRoutes.ts | 11 +++++------  1 file changed, 5 insertions(+), 6 deletions(-)

--- running-challenge-api/src/routes/authRoutes.ts
+++ running-challenge-api/src/routes/authRoutes.ts
@@ -1,10 +1,9 @@
-import { Router } from 'express';
-import { AuthController } from '../controllers/authController';
+import { Router } from "express";
+import authController from "../controllers/authController";
 
 const router = Router();
-const authController = new AuthController();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post("/register", authController.register.bind(authController));
+router.post("/login", authController.login.bind(authController));
 
 export default router;
