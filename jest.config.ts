7fe1e3cd99407d60cd13ea460c2cc8afa9cefec0
thao <mrtrados@gmail.com>
2025-08-16 20:00:12 +0900

    chore(jest.config.ts): updated\n jest.config.ts | 32 ++++++++++++++++++++++++++++++++  1 file changed, 32 insertions(+)

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,37 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/test"],
   testMatch: ["**/test/**/*.test.ts"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          module: "commonjs",
+          target: "es2020",
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          skipLibCheck: true,
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: ["node_modules/(?!(.*\\.mjs$))"],
+  moduleFileExtensions: ["ts", "js"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  extensionsToTreatAsEsm: [],
+  globals: {
+    "ts-jest": {
+      useESM: false,
+      tsconfig: {
+        module: "commonjs",
+      },
+    },
+  },
+  clearMocks: true,
+  collectCoverage: false,
+  verbose: true,
+  testTimeout: 30000,
 };
